c0c3a4404d1b7ce6b97caea76ca3da2265ef5b6c
Khyati Gandhi <khyatigandhi1998@gmail.com>
2018-06-06 21:50:13 +0530

    challenge(1)

--- app/src/main/java/com/knoxpo/khyati/geoquiz/CheatActivity.java
+++ app/src/main/java/com/knoxpo/khyati/geoquiz/CheatActivity.java
@@ -8,6 +8,7 @@ import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewAnimationUtils;
 import android.widget.Button;
@@ -15,13 +16,13 @@ import android.widget.TextView;
 
 public class CheatActivity extends AppCompatActivity {
 
-    private static final String EXTRA_ANSWER_IS_TRUE="com.knoxpo.khyati.geoquiz.answer_is_true";
-    private static final String EXTRA_ANSWER_SHOWN="com.knoxpo.khyati.geoquiz.answer_shown";
-    private static final String WAS_ANSWER_SHOWN = "false";
+    private static final String TAG="CheatActivity";
+    private static final String EXTRA_ANSWER_IS_TRUE="answer_is_true";
+    private static final String EXTRA_ANSWER_SHOWN="answer_shown";
     private boolean mAnswerIsTrue;
     private TextView mAnswerTextView;
     private Button mShowAnswerButton;
-   // private static Boolean mWasAnswerShown;
+    private Boolean mIsAnswerShown = false;
     public static Intent newIntent(Context packageContext,boolean answerIsTrue){
         Intent intent = new Intent(packageContext,CheatActivity.class);
         intent.putExtra(EXTRA_ANSWER_IS_TRUE,answerIsTrue);
@@ -33,12 +34,23 @@ public class CheatActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG,"Cheat OnCreate() called");
         setContentView(R.layout.activity_cheat);
-        /*if (savedInstanceState != null) {
-            mWasAnswerShown = savedInstanceState.getBoolean(WAS_ANSWER_SHOWN,false);
-        }*/
-        mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE,false);
         mAnswerTextView = findViewById(R.id.answer_text_view);
+
+        if (savedInstanceState != null) {
+            mAnswerIsTrue = savedInstanceState.getBoolean(EXTRA_ANSWER_IS_TRUE,false);
+            if (mAnswerIsTrue){
+                mAnswerTextView.setText(R.string.true_button);
+            }
+            else {
+                mAnswerTextView.setText(R.string.false_button);
+            }
+            setAnswerShownResult(true);
+            mIsAnswerShown = savedInstanceState.getBoolean(EXTRA_ANSWER_SHOWN,false);
+            Log.d(TAG,"answer is true "+mAnswerIsTrue+"\n answer shown "+mIsAnswerShown);
+        }
+        mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE,false);
 
         mShowAnswerButton = findViewById(R.id.show_answer_button);
         mShowAnswerButton.setOnClickListener(new View.OnClickListener() {
@@ -53,7 +65,7 @@ public class CheatActivity extends AppCompatActivity {
                 }
                 setAnswerShownResult(true);
 
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+               /* if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
                     int cx = mShowAnswerButton.getWidth() / 2;
                     int cy = mShowAnswerButton.getHeight() / 2;
                     float radius = mShowAnswerButton.getWidth();
@@ -68,14 +80,21 @@ public class CheatActivity extends AppCompatActivity {
                     anim.start();
                 }else {
                     mShowAnswerButton.setVisibility(View.INVISIBLE);
-                }
+                }*/
             }
         });
     }
     private void setAnswerShownResult(boolean isAnswerShown)
     {
+        mIsAnswerShown = isAnswerShown;
         Intent data = new Intent();
         data.putExtra(EXTRA_ANSWER_SHOWN, isAnswerShown);
         setResult(RESULT_OK,data);
+    }
+    public void onSaveInstanceState(Bundle saveInstanceState) {
+        super.onSaveInstanceState(saveInstanceState);
+        Log.i(TAG, "onSaveInstanceState() ");
+        saveInstanceState.putBoolean(EXTRA_ANSWER_IS_TRUE, mAnswerIsTrue);
+        saveInstanceState.putBoolean(EXTRA_ANSWER_SHOWN,mIsAnswerShown);
     }
 }
